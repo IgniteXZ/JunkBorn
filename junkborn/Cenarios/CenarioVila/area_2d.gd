74eddd13686443dc83954c6b7f3169effc825c4a
Jhonata <jhonatademar@gmail.com>
2026-08-25 11:44:17 -0300

    abrir mapa transicao e posicao do icone
    gilerme ajudo blz

---
+++ junkborn/Cenarios/CenarioVila/area_2d.gd
@@ -0,0 +1,20 @@
+extends Area2D
+var perto: bool = false
+
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	if perto and Input.is_action_just_pressed("Interagir"):
+		print("gonnei")
+		get_tree().change_scene_to_file("res://Deserto1.tscn")
+
+
+func _on_area_entered(area: Area2D) -> void:
+	perto = true
+	
+
+
+func _on_area_exited(area: Area2D) -> void:
+	perto = false
