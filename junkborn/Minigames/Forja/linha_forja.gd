8df24e0f9d0383459809ca204618cb8f4953fc7b
Murirebastrintas <murilo.moraisgo9@gmail.com>
2026-09-03 21:29:54 -0300

    Forja_beta_version
    Forja quase pronto so falta arrumar o erro da receita.

--- junkborn/Minigames/Forja/linha_forja.gd
+++ junkborn/Minigames/Forja/linha_forja.gd
@@ -3,7 +3,7 @@ class_name LinhaForja
 
 const SLOT_MATERIAL_SCENE = preload("res://Minigames/Forja/slotMaterial.tscn")
 
-@onready var icone_resultado = $IconeResultado
+@onready var icone_resultado = $iconeResultado
 @onready var container_materiais = $containerMateriais
 
 var slots_da_linha: Array[SlotMaterial] = []
@@ -19,29 +19,30 @@ func configurar_linha(receita: RecipeData):
 	var lista_ingredientes = receita.ingredientes.keys()
 	
 	for i in range(lista_ingredientes.size()):
-		var item = lista_ingredientes[i]
-		var quantidade_necessaria = receita.ingredientes[item]
+		var imagem_item = lista_ingredientes[i]
+		var quantidade_necessaria = receita.ingredientes[imagem_item]
 		
 		var novo_slot = SLOT_MATERIAL_SCENE.instantiate()
-		container_materiais.add_child(novo_slot)
 		
-
-		slots_da_linha.append(novo_slot)
-		
-
-		novo_slot.textura_obrigatoria = item.icone
+		novo_slot.textura_obrigatoria = imagem_item
 		novo_slot.quantidade_obrigatoria = quantidade_necessaria
 		
-
-		novo_slot.get_node("IconeMaterial").texture = item.icone
-		novo_slot.get_node("IconeMaterial").modulate = Color(0.2, 0.2, 0.2, 0.6)
-		novo_slot.get_node("textoQuantidade").text = "x" + str(quantidade_necessaria)
+		container_materiais.add_child(novo_slot)
+		slots_da_linha.append(novo_slot)
+		
+		var icone_slot = novo_slot.get_node_or_null("IconeMaterial")
+		var texto_slot = novo_slot.get_node_or_null("textoQuantidade")
+		
+		if icone_slot:
+			icone_slot.texture = imagem_item
+			icone_slot.modulate = Color(1, 1, 1, 0.8)
+		if texto_slot:
+			texto_slot.text = "0/" + str(quantidade_necessaria)
 		
 		if i < lista_ingredientes.size() - 1:
 			var sinal_mais = Label.new()
 			sinal_mais.text = "+"
 			container_materiais.add_child(sinal_mais)
-
 
 func verificar_linha_completa() -> bool:
 	for slot in slots_da_linha:
