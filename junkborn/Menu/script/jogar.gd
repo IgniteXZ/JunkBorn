115aad99a4e936d8bacfcd4d466c76bc3f196577
Jhonata <jhonatademar@gmail.com>
2026-09-08 20:00:44 -0300

    menu sair e jogar

---
+++ junkborn/Menu/script/jogar.gd
@@ -0,0 +1,35 @@
+extends Button
+
+@onready var seta = $SetaFoco
+
+var cor_normal = Color("#a6591a")
+var cor_foco = Color.WHITE
+
+
+func _ready() -> void:
+	grab_focus()
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://Player/cenas/characterP.tscn")
+
+
+func _on_mouse_entered() -> void:
+	grab_focus()
+
+
+func _on_focus_entered() -> void:
+	seta.visible = true
+	mudar_cor(cor_foco)
+
+
+func _on_focus_exited() -> void:
+	seta.visible = false
+	mudar_cor(cor_normal)
+
+
+func mudar_cor(cor: Color) -> void:
+	add_theme_color_override("font_color", cor)
+	add_theme_color_override("font_hover_color", cor)
+	add_theme_color_override("font_focus_color", cor)
+	add_theme_color_override("font_pressed_color", cor)
---
+++ junkborn/Menu/script/sair.gd
@@ -0,0 +1,34 @@
+extends Button
+
+@onready var seta = $SetaFoco
+
+var cor_normal = Color("#a6591a")
+var cor_foco = Color.WHITE
+
+
+func _ready() -> void:
+	pass
+
+func _on_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_mouse_entered() -> void:
+	grab_focus()
+
+
+func _on_focus_entered() -> void:
+	seta.visible = true
+	mudar_cor(cor_foco)
+
+
+func _on_focus_exited() -> void:
+	seta.visible = false
+	mudar_cor(cor_normal)
+
+
+func mudar_cor(cor: Color) -> void:
+	add_theme_color_override("font_color", cor)
+	add_theme_color_override("font_hover_color", cor)
+	add_theme_color_override("font_focus_color", cor)
+	add_theme_color_override("font_pressed_color", cor)
