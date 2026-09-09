c99905d2fb752ffa61497028349ee2473663dd41
kuser9cyber <kuraoka980@gmail.com>
2026-09-09 10:17:59 -0300

    Drone Tem tempo para impulso
    Co-Authored-By: luisedtancredo-prog <270462244+luisedtancredo-prog@users.noreply.github.com>

--- junkborn/Inimigo/Drone/drone.gd
+++ junkborn/Inimigo/Drone/drone.gd
@@ -4,29 +4,47 @@ var OndeEuvou: Vector2
 var Perseguicao: bool = false
 
 var ImpulsoMaster: float = 5
+var impulso: bool = false
 
 @export var Tempo: Timer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	IniciarTempo()
 
 
 func _physics_process(delta: float) -> void:
 	
-	if Perseguicao and linear_velocity.length() < 255.5:
+	if Perseguicao and linear_velocity.length() < 255.5 and not impulso:
 		var direction = global_position.direction_to(OndeEuvou)
 		apply_central_impulse(direction * ImpulsoMaster)
 		sleeping = false
 		Tempo.paused = false
+		
+	if Perseguicao and impulso:
+		await  get_tree().create_timer(0.4).timeout
+		var direction = global_position.direction_to(OndeEuvou)
+		apply_central_impulse(direction * 25)
+		sleeping = false
+		Tempo.paused = false
+		impulso = false
+		
 	
 	if Perseguicao:
 		look_at(OndeEuvou)
 		
-	elif linear_velocity.length() < 2:
-		Tempo.stop()
-		sleeping = true
 	
+		
+	elif linear_velocity.length() < 2:
+		#Tempo.stop()
+		Tempo.paused = true
+		sleeping = true
 
+
+func IniciarTempo():
+	Tempo.start()
+
+#SInais
 func _on_detectar_mandar_aura(posicaoGlobal: Vector2) -> void:
 	OndeEuvou = posicaoGlobal
 	Perseguicao = true
@@ -37,5 +55,5 @@ func _on_detectar_tirar_aura(Tirar: bool) -> void:
 
 
 func _on_timer_timeout() -> void:
-	ImpulsoMaster = 50
-	print("Sigma")
+	impulso = true
+	
--- junkborn/Inimigo/Drone/teste_drone.gd
+++ junkborn/Inimigo/Drone/teste_drone.gd
@@ -3,7 +3,7 @@ extends Node2D
 var drone = preload("res://Inimigo/Drone/drone.tscn")
 # Called when the node enters the scen$Dronee tree for the first time.
 func _ready() -> void:
-	for i in 5:
+	for i in 1:
 		var a = drone.instantiate()
 		add_child(a)
 		
