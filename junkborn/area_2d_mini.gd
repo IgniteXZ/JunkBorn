e05e9d5719a17f94b78dd4f4c8de3895fe84ce94
Jhonata <jhonatademar@gmail.com>
2026-09-14 11:05:45 -0300

    arrumando entrada minigame

--- junkborn/area_2d_mini.gd
+++ junkborn/area_2d_mini.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if perto and Input.is_action_just_pressed("Interagir"):
 		print("Aperte E para entrar na forja")
-		get_tree().change_scene_to_file("res://Minigames/Coleta/mini_game_coleta.tscn")
+		$MiniGameColeta.show()
 		
 		
 
