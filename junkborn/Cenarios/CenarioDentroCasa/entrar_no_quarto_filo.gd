6ebb7642be217881a27462f93d6d6503696e1109
luisedtancredo-prog <luisedtancredo@gmail.com>
2026-09-17 10:20:04 -0300

    ErroInteragirCorrigido

--- junkborn/Cenarios/CenarioDentroCasa/entrar_no_quarto_filo.gd
+++ junkborn/Cenarios/CenarioDentroCasa/entrar_no_quarto_filo.gd
@@ -2,6 +2,7 @@ extends Area2D
 var perto: bool = false
 var player: Area2D = null
 
+
 @export var labelInteragir: Label
 
 # Called when the node enters the scene tree for the first time.
@@ -14,6 +15,7 @@ func _process(delta: float) -> void:
 	if perto and Input.is_action_just_pressed("Interagir"):
 			print("gonnei")
 			get_tree().change_scene_to_file("res://Cenarios/CenarioDentroCasa/QuartoFilo.tscn")
+			
 
 
 func _on_area_entered(area: Area2D) -> void:
