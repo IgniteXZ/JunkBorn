602a6220429229ee4c4fe41c9a490a1d50e9b8c6
Murirebastrintas <murilo.moraisgo9@gmail.com>
2026-09-17 09:11:06 -0300

    Npcs

---
+++ junkborn/Player/cenas/npc_6.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+func _ready() -> void:
+	$AnimatedSprite2D.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
