ef693d984cc1b1e65665d13cf785dc2f9c819760
kuser9cyber <kuraoka980@gmail.com>
2026-09-23 10:20:46 -0300

    Estanho, obersar jogador

--- junkborn/Player/cenas/npc_6.gd
+++ junkborn/Player/cenas/npc_6.gd
@@ -2,8 +2,3 @@ extends CharacterBody2D
 
 func _ready() -> void:
 	$AnimatedSprite2D.play()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
