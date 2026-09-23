9ce1b70c24ae61a0cb3dd042a1e1caa30e195724
Jhonata <jhonatademar@gmail.com>
2026-09-23 10:18:53 -0300

    arrumando telas

--- junkborn/Menu/script/controles.gd
+++ junkborn/Menu/script/controles.gd
@@ -10,3 +10,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_ver_controles_pressed() -> void:
+	$"../Panel".hide()
+	$Controles.show()
+
+func _on_controles_voltar_controles_pressionado() -> void:
+	$Controles.hide()
+	$"../Panel".show()
